963405053cee197c819ead06027921c531907312
JewishLewish <lanbrokhman@gmail.com>
2023-10-21 16:19:28 -0700

    image doesn't need .svg at end

--- assets/js/getimage.js
+++ assets/js/getimage.js
@@ -6,7 +6,7 @@ console.log(param1Value);
             console.log("param1: " + param1Value);
 
             // Construct the image URL based on the parameter value
-            const imageUrl = `assets/img/${param1Value}`; // Assuming the images are in a folder named "images"
+            const imageUrl = `assets/img/${param1Value}.svg`; // Assuming the images are in a folder named "images"
 
             // Create an image element and set its source
             const image = document.createElement("img");
